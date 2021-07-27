de73bfbeb74cfbe3a5d068588130f1cd0e415ba6
Matthew Hunter <matthewhunter@Matthews-MBP.attlocal.net>
2021-07-27 13:38:12 -0400

    Initial Commit

---
+++ zombie_shooter.py
@@ -0,0 +1,25 @@
+import sys 
+
+import pygame
+
+class ZombieShooter:
+    """Overall class to manage game assets and behavior"""
+
+    def __init__(self) -> None:
+        """Initilize the game and create game resouces"""
+    pygame.init()
+
+    self.screen = pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("ZombieShooter")
+
+    def run_game(self): 
+        """Start the main loop for the game."""
+        while True: 
+            # Watch for keyboard and mouse event.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
